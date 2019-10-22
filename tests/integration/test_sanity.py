d43f2b8620d7e14691d14da0c82b900966acac7e
Assaf Giladi <assaf@weka.io>
2019-10-22 17:56:21 +0300

    add configurarion file instead of using conf.json

--- tests/integration/test_sanity.py
+++ tests/integration/test_sanity.py
@@ -1,10 +1,15 @@
 import unittest
 
+from talker_agent.talker import Config
 from talker.errors import CommandAbortedByOverflow
 from tests.utils import get_talker_client, get_retcode, get_stdout
 
 
 class IntegrationTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.host_id = Config('talker_agent/config.ini').parser.get('agent', 'host_id')
+
     def setUp(self):
         super().setUp()
         self.client = get_talker_client()
@@ -14,12 +19,12 @@ class IntegrationTest(unittest.TestCase):
         self.client.redis.flushall()
 
     def test_echo_hello_command(self):
-        cmd = self.client.run('MyHostId', 'bash', '-ce', 'echo hello')
+        cmd = self.client.run(self.host_id, 'bash', '-ce', 'echo hello')
         result = cmd.result()
         self.assertEqual(result, 'hello\n')
 
     def test_max_output_exceeds_maximum(self):
-        cmd = self.client.run('MyHostId', 'bash', '-ce', 'yes')
+        cmd = self.client.run(self.host_id, 'bash', '-ce', 'yes')
         with self.assertRaises(CommandAbortedByOverflow):
             cmd.result()
 
@@ -27,7 +32,7 @@ class IntegrationTest(unittest.TestCase):
         max_output_per_channel = 3
         for val, expected_ret in [('123', '0'), ('1234', 'overflowed')]:
             cmd = self.client.run(
-                'MyHostId', 'bash', '-ce', 'echo -n {}'.format(val), max_output_per_channel=max_output_per_channel)
+                self.host_id, 'bash', '-ce', 'echo -n {}'.format(val), max_output_per_channel=max_output_per_channel)
             ret = get_retcode(self.client.redis, cmd.job_id)
             self.assertEqual(ret, expected_ret)
             if expected_ret == 0:
